68b7a71d6f79f169fd8354c36c2c9d715e1d3ca0
ID0x6e656d6f <zaganelli.alessandro.wm@gmail.com>
2025-08-18 14:28:46 +0200

    useSearchParams Suspense missing

--- src/app/admin/edit/page.tsx
+++ src/app/admin/edit/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react"
+import { Suspense, useEffect, useState } from "react"
 
 import { getProduct } from "@/lib/supabase/getElementById"
 import { Product } from "@/types/BaseProduct"
@@ -12,107 +12,113 @@ import SubmitButton from "@/components/admin/SubmitButton"
 
 
 export default function EditPage() {
-
-
-    const params = useSearchParams()
-    const table = params.get('table') ?? ''
-    const id = Number.parseInt(params.get('id') ?? '0')
-    const [product, setProduct] = useState<Product | null>()
-    const [name, setName] = useState("")
-    const [ingredients, setIngredients] = useState("")
-    const [price, setPrice] = useState("")
-    const [productType, setProductType] = useState(table)
-    const [type, setType] = useState("bianco");
-
-    useEffect(() => {
-        async function loadAll() {
-            const product = await getProduct(table, id)
-            if (product) {
-                setProduct(product)
-                setName(product.name)
-                setIngredients(product.ingredients)
-                setType(product.type)
-                setPrice(product.price.toString())
-            }
-        }
-        loadAll()
-    }, [])
-
-    const handleChange = async (e: React.FormEvent) => {
-        e.preventDefault()
-
-        if (productType === "drinks" || productType == "analcoholic" || productType == "boards" || productType == "carpaccio" || productType === "baked_potatoes" || productType === "schiacce" || productType === "glass_wine" || productType === "bottled_wine" || productType === "beers") {
-            const { error } = await supabase.from(productType).update([
-                {
-                    "name": name,
-                    "ingredients": ingredients,
-                    "price": parseFloat(price),
-                },
-            ]).eq("id", product?.id)
-            if (error) {
-                alert("errore: " + error.message)
-            } else {
-                alert(name + " aggiornato con successo")
-            }
-        } else if (productType === "whiskey" || productType === "rum" || productType == "liqueur" || productType == "gin" || productType === "draft_drinks") {
-            const { error } = await supabase.from(productType).update(
-                {
-                    "name": name,
-                    "price": parseFloat(price),
-                },
-            )
-            if (error) {
-                alert("errore: " + error.message)
-            } else {
-                alert(name + " aggiornato con successo")
-            }
-        }
-    }
-
     return (
-        <div>
-            {(product != null) && (
-                <div className="p-5 flex flex-col items-center justify-center min-h-screen w-full">
-                    <form onSubmit={handleChange}>
-                        <Input
-                            label="NOME"
-                            value={name}
-                            onChange={setName}
-                            type="text"
-                        />
-                        {(productType === "drinks" || productType === "schiacce" || productType == "analcoholic" || productType == "boards" || productType == "carpaccio" || productType === "baked_potatoes") && (
-                            <>
-                                <Input
-                                    label="INGREDIENTS"
-                                    value={ingredients}
-                                    onChange={setIngredients}
-                                    type="text"
-                                />
-                            </>
-                        )}
+        <Suspense fallback={<div>Loading...</div>}>
+            <Content />
+        </Suspense>
+    );
 
-                        {(productType === "beers" || productType === "bottled_wine" || productType == "glass_wine" || productType == "draft_beers") && (
-                            <>
-                                <Input
-                                    label="TYPE"
-                                    value={type}
-                                    onChange={setType}
-                                    type="text"
-                                />
-                            </>
-                        )}
-                        < Input
-                            label="PREZZO"
-                            value={price}
-                            onChange={setPrice}
-                            type="number"
-                        />
-                        <SubmitButton
-                            label="AGGIORNA PRODOTTO"
-                        />
-                    </form>
-                </div>
-            )}
-        </div>
-    )
+    function Content() {
+        const params = useSearchParams()
+        const table = params.get('table') ?? ''
+        const id = Number.parseInt(params.get('id') ?? '0')
+        const [product, setProduct] = useState<Product | null>()
+        const [name, setName] = useState("")
+        const [ingredients, setIngredients] = useState("")
+        const [price, setPrice] = useState("")
+        const [productType, setProductType] = useState(table)
+        const [type, setType] = useState("bianco");
+
+        useEffect(() => {
+            async function loadAll() {
+                const product = await getProduct(table, id)
+                if (product) {
+                    setProduct(product)
+                    setName(product.name)
+                    setIngredients(product.ingredients)
+                    setType(product.type)
+                    setPrice(product.price.toString())
+                }
+            }
+            loadAll()
+        }, [])
+
+        const handleChange = async (e: React.FormEvent) => {
+            e.preventDefault()
+
+            if (productType === "drinks" || productType == "analcoholic" || productType == "boards" || productType == "carpaccio" || productType === "baked_potatoes" || productType === "schiacce" || productType === "glass_wine" || productType === "bottled_wine" || productType === "beers") {
+                const { error } = await supabase.from(productType).update([
+                    {
+                        "name": name,
+                        "ingredients": ingredients,
+                        "price": parseFloat(price),
+                    },
+                ]).eq("id", product?.id)
+                if (error) {
+                    alert("errore: " + error.message)
+                } else {
+                    alert(name + " aggiornato con successo")
+                }
+            } else if (productType === "whiskey" || productType === "rum" || productType == "liqueur" || productType == "gin" || productType === "draft_drinks") {
+                const { error } = await supabase.from(productType).update(
+                    {
+                        "name": name,
+                        "price": parseFloat(price),
+                    },
+                )
+                if (error) {
+                    alert("errore: " + error.message)
+                } else {
+                    alert(name + " aggiornato con successo")
+                }
+            }
+        }
+
+        return (
+            <div>
+                {(product != null) && (
+                    <div className="p-5 flex flex-col items-center justify-center min-h-screen w-full">
+                        <form onSubmit={handleChange}>
+                            <Input
+                                label="NOME"
+                                value={name}
+                                onChange={setName}
+                                type="text"
+                            />
+                            {(productType === "drinks" || productType === "schiacce" || productType == "analcoholic" || productType == "boards" || productType == "carpaccio" || productType === "baked_potatoes") && (
+                                <>
+                                    <Input
+                                        label="INGREDIENTS"
+                                        value={ingredients}
+                                        onChange={setIngredients}
+                                        type="text"
+                                    />
+                                </>
+                            )}
+
+                            {(productType === "beers" || productType === "bottled_wine" || productType == "glass_wine" || productType == "draft_beers") && (
+                                <>
+                                    <Input
+                                        label="TYPE"
+                                        value={type}
+                                        onChange={setType}
+                                        type="text"
+                                    />
+                                </>
+                            )}
+                            < Input
+                                label="PREZZO"
+                                value={price}
+                                onChange={setPrice}
+                                type="number"
+                            />
+                            <SubmitButton
+                                label="AGGIORNA PRODOTTO"
+                            />
+                        </form>
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
